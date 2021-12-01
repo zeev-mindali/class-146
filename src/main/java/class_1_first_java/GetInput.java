b9254ef52e72dc3e59df35893c35803c81ac6a74
zeev mindali <mindalizeev@gmail.com>
2021-12-01 14:54:30 +0200

    our first program

---
+++ src/main/java/class_1_first_java/GetInput.java
@@ -0,0 +1,17 @@
+package class_1_first_java;
+
+import java.util.Scanner;
+
+public class GetInput {
+    public static void main(String[] args) {
+        //creating an object to get input
+        Scanner input = new Scanner(System.in);
+
+        //write a message to the client
+        System.out.println("Please enter a number:");
+        //get the input from client
+        int myNumber = input.nextInt();
+        //print the input to the screen
+        System.out.println("your number is:"+myNumber);
+    }
+}
---
+++ src/main/java/class_1_first_java/Tester.java
@@ -0,0 +1,19 @@
+package class_1_first_java;
+
+public class Tester {
+    //the java will run this method first.....
+    public static void main(String[] args) {
+        //start
+        // remark line
+        System.out.println("Hello world !!!!!");
+        System.out.println("=================");
+        System.out.println("Hello Class 146");
+        /*
+            this is my first program
+            now i have someting to show mom
+            i will write google
+         */
+
+        //end
+    }
+}
