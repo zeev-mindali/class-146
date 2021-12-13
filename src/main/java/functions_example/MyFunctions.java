a095c5f6c6e0e35b8ef5812edef75e519aaffcaa
zeev mindali <mindalizeev@gmail.com>
2021-12-13 15:37:14 +0200

    loops

---
+++ src/main/java/functions_example/MyFunctions.java
@@ -0,0 +1,36 @@
+package functions_example;
+
+import java.util.Scanner;
+
+public class MyFunctions {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = input.nextInt();
+        int numberLength = findLength(number);
+        System.out.println("number length:"+findLength(number));
+        helloGery();
+
+    }
+
+    public static int findLength(int userNumber){
+        int counter=0;
+        while (userNumber>0){
+            counter++;
+            userNumber/=10;
+        }
+        return counter;
+    }
+
+    public static void helloGery(){
+        String name = "██████╗ ███████╗██████╗ ██╗   ██╗\n" +
+                "██╔════╝ ██╔════╝██╔══██╗╚██╗ ██╔╝\n" +
+                "██║  ███╗█████╗  ██████╔╝ ╚████╔╝ \n" +
+                "██║   ██║██╔══╝  ██╔══██╗  ╚██╔╝  \n" +
+                "╚██████╔╝███████╗██║  ██║   ██║   \n" +
+                " ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   \n" +
+                "                                  ";
+        System.out.println(name);
+
+    }
+}
