961e968ecb053cbae5a8014e2de2a318d67a8a94
zeev mindali <mindalizeev@gmail.com>
2021-12-15 15:11:49 +0200

    loops

---
+++ src/main/java/arrays/FirstExample.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        final int ARRAY_SIZE=4;
+//        String[] names = new String[ARRAY_SIZE];
+//        names[0] = "Zeev";
+//        names[1] = "Omer";
+//        names[2] = "Saar";
+//        names[3] = "Tal";
+        String[] names = {"Zeev","Omer","Saar","Tal"};
+
+        //System.out.printf("names: %s %s %s",names[0],names[1],names[2]);
+//        for (int counter = 0; counter < names.length; counter++) {
+//            System.out.println(names[counter]);
+//        }
+        System.out.println(names);
+    }
+}
