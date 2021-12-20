661cb3b7432db85224cef91cb10633338fd62c0b
zeev mindali <mindalizeev@gmail.com>
2021-12-20 15:41:31 +0200

    loops_solution

---
+++ src/main/java/oop/BankTester.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class BankTester {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        System.out.println(bank.login("admin","zeev"));
+
+        System.out.println(bank.givePassword());
+    }
+}
