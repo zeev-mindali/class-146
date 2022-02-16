fe97cae4859d7ccd7592032e9eebee3caf9f92ad
zeev mindali <mindalizeev@gmail.com>
2022-02-16 15:18:28 +0200

    json example

--- src/main/java/xml_json/Tester.java
+++ src/main/java/xml_json/Tester.java
@@ -1,5 +1,8 @@
 package xml_json;
 
+//json object
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -13,20 +16,54 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Tester {
     public static final String XML_URL = "https://www.boi.org.il/currency.xml";
-    public static final String JSON_URL = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=1113334";
-
+    public static final String JSON_URL = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=";
+    public static final String JSON_BIKE_URL = "https://data.gov.il/api/3/action/datastore_search?resource_id=bf9df4e2-d90d-4c0a-a400-19e15af8e95f&q=";
     public static void main(String[] args) {
         Tester tester = new Tester();
+        Scanner scanner = new Scanner(System.in);
+
         //System.out.println(tester.getURLdata(XML_URL));
         List<Currency> currencies = tester.readXML(tester.getURLdata(XML_URL));
         //currencies.forEach(System.out::println);
-        currencies.stream()
-                .filter(item->item.getChange()<0)
-                .filter(item->item.getName().equals("Dollar"))
-                .forEach(System.out::println);
+//        currencies.stream()
+//                .filter(item->item.getChange()<0)
+//                .filter(item->item.getName().equals("Dollar"))
+//                .forEach(System.out::println);
+        System.out.println("Please enter car plate number:");
+        String carNumber = scanner.nextLine();
+        //tester.readJSON(tester.getURLdata(JSON_URL+carNumber));
+        tester.readJSON(tester.getURLdata(JSON_BIKE_URL+carNumber));
+    }
+
+    private void readJSON(String myJson){
+        //convert our string to json object
+        try {
+            //convert our String to json object (don't forget to import sax.json
+            var carData = new JSONObject(myJson);
+            //getting our result from json object;
+            var result = carData.getJSONObject("result");
+            //get json array from result
+            var carArry = result.getJSONArray("records");
+            if (carArry.length()==0){
+                System.out.println("Car not found");
+                return;
+            }
+            //get a single car from the array
+            var singleCar = (JSONObject) carArry.get(0);
+
+            System.out.println("mispar rechev: "+singleCar.getInt("mispar_rechev"));
+            System.out.println("manufactor: "+singleCar.getString("tozeret_nm"));
+            System.out.println("model: "+singleCar.getString("kinuy_mishari"));
+            System.out.println("test: "+singleCar.getString("tokef_dt").split("T")[0]);
+            System.out.println("tzeva rechev: "+singleCar.getString("tzeva_rechev"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
