5a7d3a12eb83162790b23ffc3e2643708d87e194
zeev mindali <mindalizeev@gmail.com>
2022-06-15 11:34:08 +0300

    some java stuff

--- app/src/main/java/com/jb/mycalculator/MainActivity.java
+++ app/src/main/java/com/jb/mycalculator/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView sum;
     private RelativeLayout num1;
+    private char opr = ' ';
+    private double totalSum = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setPointer();
     }
 
-    private void setPointer(){
+    private void setPointer() {
         sum = findViewById(R.id.sum);
         //change the text inside the text view
         //sum.setText("Zeev Mindali");
@@ -45,13 +47,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         double myNum = Double.parseDouble(sum.getText().toString());
 
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.num1:
-                myNum=myNum*10+1;
+                myNum = myNum * 10 + 1;
+                break;
+            case R.id.num2:
+                myNum = myNum * 10 + 2;
+                break;
+            case R.id.num3:
+                myNum = myNum * 10 + 3;
+                break;
+            case R.id.num4:
+                myNum = myNum * 10 + 4;
+                break;
+            case R.id.num5:
+                myNum = myNum * 10 + 5;
+                break;
+            case R.id.num6:
+                myNum = myNum * 10 + 6;
+                break;
+            case R.id.num7:
+                myNum = myNum * 10 + 7;
+                break;
+            case R.id.num8:
+                myNum = myNum * 10 + 8;
+                break;
+            case R.id.num9:
+                myNum = myNum * 10 + 9;
+                break;
+            case R.id.num0:
+                myNum = myNum * 10;
                 break;
 
+            //operations : /-div *-mul - min + plus
+            case R.id.div:
+
+                break;
+
+            case R.id.mul:
+
+                break;
+
+            case R.id.min:
+
+                break;
+            case R.id.plus:
+                totalSum = myNum;
+                opr = '+';
+                myNum = 0;
+                break;
             case R.id.clr:
-                myNum=0;
+                myNum = 0;
+
+            case R.id.eql:
+                switch (opr) {
+                    case '+':
+                        myNum+=totalSum;
+                        break;
+                    case '-':
+
+                        break;
+
+                    case '*':
+
+                        break;
+
+                    case '/':
+
+                        break;
+                    default:
+                        sum.setText("WTF??");
+                        return;
+                }
         }
 
         sum.setText(String.valueOf(myNum));
